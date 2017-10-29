af53ba81ac5b124002a9beeb3b0b9ab6cfe5455b
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2017-10-29 17:03:34 +0200

    connui-scanbox-view: Fix coding style and possible double-free
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- lib/connui-scan-box-view.c
+++ lib/connui-scan-box-view.c
@@ -36,75 +36,85 @@ G_DEFINE_TYPE(ConnuiScanBoxView, connui_scan_box_view, CONNUI_TYPE_BOX_VIEW);
 G_DEFINE_TYPE_WITH_PRIVATE(ConnuiScanBoxView, connui_scan_box_view, CONNUI_TYPE_BOX_VIEW);
 #endif
 
-static void add_icon_to_button(GtkWidget *button, GdkPixbuf *icon)
+static void
+add_icon_to_button(GtkWidget *button, GdkPixbuf *icon)
 {
   GtkWidget *image;
+  GtkWidget *parent;
+
   if (icon)
     image = gtk_image_new_from_pixbuf(icon);
   else
     image = gtk_image_new();
-  iap_common_pack_to_hildon_button(button, image, 0, 0);
-  if (G_TYPE_CHECK_INSTANCE_TYPE(gtk_widget_get_parent(image),GTK_TYPE_BOX))
-  {
-    gtk_widget_show_all(gtk_widget_get_parent(image));
-  }
+
+  iap_common_pack_to_hildon_button(button, image, FALSE, FALSE);
+
+  parent = gtk_widget_get_parent(image);
+
+  if (GTK_IS_BOX(parent))
+    gtk_widget_show_all(parent);
   else
-  {
     gtk_widget_destroy(image);
-  }
 }
-static void connui_scan_box_view_update_child(ConnuiBoxView *view, GtkWidget *hbutton, GtkTreeModel *model, GtkTreeIter *iter)
+static void
+connui_scan_box_view_update_child(ConnuiBoxView *view, GtkWidget *hbutton,
+                                  GtkTreeModel *model, GtkTreeIter *iter)
 {
-  gchararray title = 0;
-  gchararray icon1 = 0;
-  gchararray icon2 = 0;
-  gchararray icon3 = 0;
-  network_entry *entry = 0;
-  gboolean can_disconnect;
+  gchar *title = NULL;
+  gchar *icon1 = NULL;
+  gchar *icon2 = NULL;
+  gchar *icon3 = NULL;
+  GdkPixbuf *icon1buf;
+  GdkPixbuf *icon2buf;
+  GdkPixbuf *icon3buf;
+  network_entry *entry = NULL;
+  gboolean can_disconnect = FALSE;
   ConnuiScanBoxViewPrivate *priv = CONNUI_SCAN_BOX_VIEW(view)->priv;
-  gtk_tree_model_get(model, iter, 0, &icon1, 4, &title, 7, &icon2, 9, &icon3, 10, &entry, 11, &can_disconnect, -1);
-  if (title)
+
+  gtk_tree_model_get(model, iter, 0, &icon1, 4, &title, 7, &icon2, 9,
+                     &icon3, 10, &entry, 11, &can_disconnect, -1);
+
+  if (!title)
   {
-    GdkPixbuf *icon1buf = connui_pixbuf_cache_get(priv->cache, icon1, 48);
-    GdkPixbuf *icon2buf = connui_pixbuf_cache_get(priv->cache, icon2, 48);
-    GdkPixbuf *icon3buf = connui_pixbuf_cache_get(priv->cache, icon3, 48);
-    if (can_disconnect)
-    {
-      gchararray tmp = title;
-      title = g_strdup_printf(dcgettext("osso-connectivity-ui", "conn_fi_disconnect_iap", 5), tmp);
-      g_free(tmp);
-    }
-    hildon_button_set_title(HILDON_BUTTON(hbutton), title);
-    if (entry)
-    {
-      if (entry->network_type)
-      {
-        if (!strncmp(entry->network_type, "WLAN_", 5u))
-        {
-          if (entry->network_attributes & 0x1000)
-          {
-            hildon_button_set_value(HILDON_BUTTON(hbutton), dgettext("osso-connectivity-ui", "conn_va_wps_complient"));
-          }
-        }
-      }
-    }
-    hildon_button_set_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5, 1.0, 0.0);
-    hildon_button_set_title_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5);
-    hildon_button_set_value_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5);
-    add_icon_to_button(hbutton,icon1buf);
-    add_icon_to_button(hbutton,icon2buf);
-    add_icon_to_button(hbutton,icon3buf);
     g_free(icon1);
-    g_free(title);
     g_free(icon2);
     g_free(icon3);
+    return;
   }
-  if (icon1)
-    g_free(icon1);
-  if (icon2)
-    g_free(icon2);
-  if (icon3)
-    g_free(icon3);
+
+  icon1buf = connui_pixbuf_cache_get(priv->cache, icon1, 48);
+  icon2buf = connui_pixbuf_cache_get(priv->cache, icon2, 48);
+  icon3buf = connui_pixbuf_cache_get(priv->cache, icon3, 48);
+
+  if (can_disconnect)
+  {
+    gchar* tmp = title;
+    title = g_strdup_printf(
+          dgettext("osso-connectivity-ui", "conn_fi_disconnect_iap"), tmp);
+    g_free(tmp);
+  }
+
+  hildon_button_set_title(HILDON_BUTTON(hbutton), title);
+
+  if (entry && entry->network_type &&
+      !strncmp(entry->network_type, "WLAN_", 5) &&
+      entry->network_attributes & 0x1000)
+  {
+    hildon_button_set_value(HILDON_BUTTON(hbutton),
+                            dgettext("osso-connectivity-ui",
+                                     "conn_va_wps_complient"));
+  }
+
+  hildon_button_set_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5, 1.0, 0.0);
+  hildon_button_set_title_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5);
+  hildon_button_set_value_alignment(HILDON_BUTTON(hbutton), 0.0, 0.5);
+  add_icon_to_button(hbutton, icon1buf);
+  add_icon_to_button(hbutton, icon2buf);
+  add_icon_to_button(hbutton, icon3buf);
+  g_free(title);
+  g_free(icon1);
+  g_free(icon2);
+  g_free(icon3);
 }
 
 static void
