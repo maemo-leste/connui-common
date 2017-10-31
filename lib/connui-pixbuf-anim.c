243f70a8c69500cb023267ebf74bc29547c16e4a
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2017-10-31 09:43:39 +0200

    finish connui_pixbuf_anim
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- lib/connui-pixbuf-anim.c
+++ lib/connui-pixbuf-anim.c
@@ -145,3 +145,63 @@ connui_pixbuf_anim_show_current_and_advance(ConnuiPixbufAnim *anim)
   else
     anim->timeout = g_timeout_add(timeout, connui_pixbuf_anim_timeout, anim);
 }
+
+ConnuiPixbufAnim *
+connui_pixbuf_anim_new_from_icons(int size, float rate,
+                                  const gchar *first_icon, ...)
+{
+  GtkIconTheme *icon_theme;
+  ConnuiPixbufAnim *anim = NULL;
+  GdkPixbufSimpleAnim *simple_anim;
+  const gchar *icon_name = first_icon;
+  va_list ap;
+
+  icon_theme = gtk_icon_theme_get_default();
+
+  g_return_val_if_fail(first_icon != NULL && icon_theme != NULL, NULL);
+
+  simple_anim = gdk_pixbuf_simple_anim_new(size, size, rate);
+
+  if (!simple_anim)
+  {
+    CONNUI_ERR("Unable to create simple anim");
+    return NULL;
+  }
+
+  va_start(ap, first_icon);
+
+  while (1)
+  {
+    GError *error = NULL;
+    GdkPixbuf *icon =
+        gtk_icon_theme_load_icon(icon_theme, icon_name, size, 0, &error);
+
+    if (!icon)
+    {
+      if (error)
+        CONNUI_ERR("Unable to load icon %s: %s", icon_name, error->message);
+      else
+        CONNUI_ERR("Unable to load icon %s: unknown error", icon_name);
+
+      g_clear_error(&error);
+      g_object_unref(G_OBJECT(simple_anim));
+      goto err_out;
+    }
+
+    gdk_pixbuf_simple_anim_add_frame(simple_anim, icon);
+    g_object_unref(G_OBJECT(icon));
+
+    icon_name = va_arg(ap, const gchar *);
+
+    if (!icon_name)
+      break;
+  }
+
+  anim = g_new0(ConnuiPixbufAnim, 1);
+  anim->animation = GDK_PIXBUF_ANIMATION(simple_anim);
+
+err_out:
+  va_end(ap);
+
+  return anim;
+}
--- lib/connui-pixbuf-anim.h
+++ lib/connui-pixbuf-anim.h
@@ -12,5 +12,6 @@ void connui_pixbuf_anim_destroy(ConnuiPixbufAnim *anim);
 void connui_pixbuf_anim_start(ConnuiPixbufAnim *anim, gpointer user_data, pixbuf_anim_cb callback);
 void connui_pixbuf_anim_stop(ConnuiPixbufAnim *anim);
 void connui_pixbuf_anim_show_current_and_advance(ConnuiPixbufAnim *anim);
+ConnuiPixbufAnim *connui_pixbuf_anim_new_from_icons(int size, float rate, const gchar *first_icon, ...);
 
 #endif // CONNUI_PIXBUF_ANIM_H
