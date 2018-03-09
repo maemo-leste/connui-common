fd1d8cd04222b0d6ab9c7dc6bf0bcb91c4f0dbc9
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2018-03-09 09:17:32 +0200

    fix connui_flightmode_close declaration
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- include/connui-flightmode.h
+++ include/connui-flightmode.h
@@ -6,7 +6,7 @@ typedef void (*connui_flightmode_notify) (dbus_bool_t offline, gpointer user_dat
 dbus_bool_t connui_flightmode_off_confirm();
 dbus_bool_t connui_flightmode_off();
 dbus_bool_t connui_flightmode_on();
-dbus_bool_t connui_flightmode_close(connui_flightmode_notify callback);
+void connui_flightmode_close(connui_flightmode_notify callback);
 gboolean connui_flightmode_status(connui_flightmode_notify callback, gpointer user_data);
 
 #endif
